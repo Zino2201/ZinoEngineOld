8470e9f788d19ab2bb1e276a0cff25df4b90ce76
Zino <zineddineamazouz@gmail.com>
2020-04-26 20:17:21 +0200

    README & BuildLibs.bat update

--- BuildLibs.bat
+++ BuildLibs.bat
@@ -1,2 +1,70 @@
 @echo off
-echo Building 3rd party libs...
+
+set ROOT=%CD%
+
+color A
+
+echo Building 3rd party libs for VS2019... (requires CMake, Python 3 and Git)
+
+REM MS Build
+echo Locating VS...
+setlocal enabledelayedexpansion
+for /f "usebackq tokens=*" %%i in (`vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
+  set MSBUILD=%%i
+)
+
+if not defined MSBUILD goto :msbuildnotfound
+
+REM SPIRV-Tools
+echo Building SPIRV-Tools
+cd Sources/Libs/SPIRV-Tools/
+echo Syncing deps
+echo %ERRORLEVEL% > python3 utils/git-sync-deps 
+echo Building using CMake
+mkdir build
+cd build
+cmake -G "Visual Studio 16 2019" -A x64 ../
+echo Debug
+"%MSBUILD%" ALL_BUILD.vcxproj /t:build /p:Configuration="Debug" /p:Platform="x64"
+echo Release
+"%MSBUILD%" ALL_BUILD.vcxproj /t:build /p:Configuration="Release" /p:Platform="x64"
+
+REM SPIRV-Cross
+echo Building SPIRV-Cross
+cd ../../SPIRV-Cross/
+mkdir build
+cd build
+cmake -G "Visual Studio 16 2019" -A x64 ../
+echo Debug
+"%MSBUILD%" ALL_BUILD.vcxproj /t:build /p:Configuration="Debug" /p:Platform="x64"
+echo Release
+"%MSBUILD%" ALL_BUILD.vcxproj /t:build /p:Configuration="Release" /p:Platform="x64"
+
+REM Glslang
+echo Building glslang
+cd ../../glslang/
+mkdir build
+cd build
+cmake -G "Visual Studio 16 2019" -A x64 ../ -DENABLE_CTEST=false -DBUILD_TESTING=false -DBUILD_EXTERNAL=false -DSKIP_GLSLANG_INSTALL=true
+echo Debug
+"%MSBUILD%" ALL_BUILD.vcxproj /t:build /p:Configuration="Debug" /p:Platform="x64"
+echo Release
+"%MSBUILD%" ALL_BUILD.vcxproj /t:build /p:Configuration="Release" /p:Platform="x64"
+
+REM Shaderc
+echo Building shaderc
+cd ../../shaderc
+mkdir build
+cd build
+cmake -G "Visual Studio 16 2019" -A x64 ../ -DENABLE_CTEST=false -DBUILD_GMOCK=false -DBUILD_TESTING=false -DSHADERC_SKIP_TESTS=true -DSHADERC_SPIRV_TOOLS_DIR=%ROOT%/Sources/Libs/SPIRV-Tools -DSHADERC_GLSLANG_DIR=%ROOT%/Sources/Libs/glslang -DSHADERC_ENABLE_SHARED_CRT=true
+echo Debug
+"%MSBUILD%" ALL_BUILD.vcxproj /t:build /p:Configuration="Debug" /p:Platform="x64"
+echo Release
+"%MSBUILD%" ALL_BUILD.vcxproj /t:build /p:Configuration="Release" /p:Platform="x64"
+pause
+exit
+
+:msbuildnotfound
+echo MS Build not found
+pause
+exit
--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # ZinoEngine
 
-A custom 3d vulkan-only game engine that I make for fun.
+A custom C++ 3d vulkan only (for now) game engine that I make for fun.
+
+# Build
+
+For now only VS2019 is supported, but if you want you can try to build it for other compilers.
+
+- Run BuildLibs.bat to compile 3rd party libs
+- Run GenerateVS2019.bat to generate the solution and project files
+- Open the solution in Build/ProjectFiles and build it
+
+Only Debug is implemented for now.
