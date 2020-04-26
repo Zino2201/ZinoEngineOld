48ac02b35d06eee077b90dd3b63ea784b575bff0
Zino <zineddineamazouz@gmail.com>
2020-04-26 20:14:32 +0200

    Battle for Submodules V Removed RTTR and magic_enum

---
+++ ClearLibs.bat
@@ -0,0 +1,6 @@
+REM Deleting builds
+echo Deleting builds directories
+rd /S /Q Sources\Libs\SPIRV-Tools\build
+rd /S /Q Sources\Libs\SPIRV-Cross\build
+rd /S /Q Sources\Libs\glslang\build
+rd /S /Q Sources\Libs\shaderc\build
