1c5f28f24d1bf1673020b02245b28f2ceb0d891a
Zino <zineddineamazouz@gmail.com>
2020-06-19 16:16:40 +0200

    Fixed release mode crashes

--- Shaders/PostProcess/BarrelDistortionFS.hlsl
+++ Shaders/PostProcess/BarrelDistortionFS.hlsl
@@ -1,10 +1,15 @@
+#include "ZE.hlsl"
+
 struct VSOutput
 {
     float4 Position : SV_POSITION;
     float2 TexCoord : TEXCOORD0;
 };
 
+[[vk::binding(0, ZE_GLOBAL_SET)]]
 Texture2D Color : register(t0);
+
+[[vk::binding(1, ZE_GLOBAL_SET)]]
 SamplerState Sampler : register(s1);
 
 float4 Main(VSOutput Input) : SV_TARGET
