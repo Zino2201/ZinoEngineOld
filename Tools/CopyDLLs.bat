ee1fff83120b6977c9721367e23d6071ea217c6b
Zino <zineddineamazouz@gmail.com>
2020-08-29 19:53:32 +0200

    Fixed CI RelWithDebInfo Editor artifact

--- Tools/CopyDLLs.bat
+++ Tools/CopyDLLs.bat
@@ -6,8 +6,8 @@ cd ../
 mkdir Binaries\Debug\ 2>NUL
 mkdir Binaries\Release\ 2>NUL
 mkdir Binaries\RelWithDebInfo\ 2>NUL
+mkdir Binaries\RelWithDebInfo-Editor\ 2>NUL
 mkdir Binaries\ReleaseMonolithic\ 2>NUL
-mkdir Binaries\RelWithDebInfoMonolithic\ 2>NUL
 
 copy Sources\Libs\ShaderConductor\Build\vs2019-win-vc142-x64\Bin\Debug\ShaderConductor.dll Binaries\Debug\ShaderConductor.dll
 copy Sources\Libs\ShaderConductor\Build\vs2019-win-vc142-x64\Bin\Debug\dxcompiler.dll Binaries\Debug\dxcompiler.dll
@@ -19,7 +19,7 @@ copy Sources\Libs\ShaderConductor\Build\vs2019-win-vc142-x64\Bin\Release\dxcompi
 
 copy Sources\Libs\ShaderConductor\Build\vs2019-win-vc142-x64\Bin\Release\ShaderConductor.dll Binaries\RelWithDebInfo\ShaderConductor.dll
 copy Sources\Libs\ShaderConductor\Build\vs2019-win-vc142-x64\Bin\Release\dxcompiler.dll Binaries\RelWithDebInfo\dxcompiler.dll
-copy Sources\Libs\ShaderConductor\Build\vs2019-win-vc142-x64\Bin\Release\ShaderConductor.dll Binaries\RelWithDebInfoMonolithic\ShaderConductor.dll
-copy Sources\Libs\ShaderConductor\Build\vs2019-win-vc142-x64\Bin\Release\dxcompiler.dll Binaries\RelWithDebInfoMonolithic\dxcompiler.dll
+copy Sources\Libs\ShaderConductor\Build\vs2019-win-vc142-x64\Bin\Release\ShaderConductor.dll Binaries\RelWithDebInfo-Editor\ShaderConductor.dll
+copy Sources\Libs\ShaderConductor\Build\vs2019-win-vc142-x64\Bin\Release\dxcompiler.dll Binaries\RelWithDebInfo-Editor\dxcompiler.dll
 
 pause
